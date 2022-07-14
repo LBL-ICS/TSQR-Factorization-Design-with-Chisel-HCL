647cfb855724af2f208f7c4e4272b3af29c9ec90
BenjaminDGarrett <benjamin1234567892016@outlook.com>
2022-07-14 18:53:47 -0500

    Second Draft

--- src/main/scala/GoldenModel_QR_HouseHolder.scala
+++ src/main/scala/GoldenModel_QR_HouseHolder.scala
@@ -3,15 +3,18 @@ import scala.math.sqrt
 class GoldenModel_QR_HouseHolder(row: Int, col: Int) {
   // defines the array, and the parameters
   var a, b, d = 0.0 //a for col, b for rows, d for dot product
-  var n = col
-  var m = row
-  var p = 2
+  var n = col //col
+  var m = row //row
+  var p = 2 // processor?
   var myMatrix = Array.ofDim[Double](m, n)
   var holder = Array.ofDim[Double](m)
   var reflector = Array.ofDim[Double](m)
   var trailing = Array.ofDim[Double](m)
   var final_Trailing = Array.ofDim[Double](m)
+  var TEST_MATRIX = Array.ofDim[Double](m)
   var tk = 0.0
+  var kr,jr = 0
+  // all labels with TEST_MATRIX have nothing to do with the algorithm and should be ignored, they are only meant for testing
 
   var d1, d2, d3, d4, d5 = 0.0
 
@@ -23,6 +26,17 @@ class GoldenModel_QR_HouseHolder(row: Int, col: Int) {
   }
   myMatrix(1)(1) = 4
   myMatrix(2)(2) = 7
+  TEST_MATRIX(0) = 3
+  TEST_MATRIX(1) = 5
+  TEST_MATRIX(2) = 6
+
+
+  //for (d <- 1 until m) {
+    //for (b <- 1 until m) {
+      //d1 = TEST_MATRIX(d) * TEST_MATRIX(b) + d1
+    //}
+  //}
+//print(d1)
 
 
   //for (a <- 0 until m) {
@@ -32,84 +46,81 @@ class GoldenModel_QR_HouseHolder(row: Int, col: Int) {
    // println("")
   //}
 
-  for (k <- 1 until n - 1) {
+  for (k <- 1 until 2) {
     //hqr1 get the next section x
-      for (b <- 0 until m) {
-        //!issue dealing with the k value
-        holder(b) = myMatrix(b)(k)
+    kr = k - 1
+    for (b <- 0 until m) {
+      holder(b) = myMatrix(b)(kr)
+      //println(holder(b))
     }
     b = 0;
     //hqr2 ddot product
-    for (d <- 0 until m) {
-      for (b <- 0 until m) {
+    for (d <- kr until m) {
+      for (b <- kr until m) {
         d1 = holder(d) * holder(b) + d1
+
       }
-      b = 0
     }
     d = 0
+
     //hqr3
     d2 = sqrt(d1)
+
     //hqr4
     holder.copyToArray(reflector)
     //hqr5
-    if (holder(1) < 0) {
-      reflector(1) = holder(1) + -d2
+    if (holder(0) < 0) {
+      reflector(0) = holder(0) + -d2
     }
-    else{
-      reflector(1) = holder(1) + d2
+    else {
+      reflector(0) = holder(0) + d2
     }
     //hqr6
-    for (d <- 0 until m) {
-      for (b <- 0 until m) {
+    for (d <- kr until m) {
+      for (b <- kr until m) {
         d3 = reflector(d) * reflector(b) + d3
       }
-      b = 0
     }
     d = 0
     //hqr7
-    tk = -2/d3
+    tk = -2 / d3
 
-    for (j <- k until n){
-
+    for (j <- k until n + 1) {
+      jr = j - 1
       //hqr8
       for (b <- 0 until m) {
-        trailing(b) = myMatrix(b)(j)
-        //println(trailing(b));
+        trailing(b) = myMatrix(b)(jr)
+        //print(trailing(b))
       }
 
 
       //hqr9
-      for (d <- 0 until m) {
-        for (b <- 0 until m) {
+      for (d <- jr until m) {
+        for (b <- jr until m) {
           //yT times x
-          d4 = trailing(b) * reflector(d) + d4
+          d4 = trailing(d) * reflector(b) + d4
         }
-        b = 0
       }
       d = 0
-
+      //print(d4)
       //hqr10
-      for (b <- 0 until m) {
-        reflector(b)=  reflector(b) * d4
-      }
-      for (b <- 0 until m) {
-        final_Trailing(b)=  reflector(b) +  trailing(b)
-      }
-
-
+      d5 = tk * d4
       //hqr11
+      for (b <- jr until m) {
+        final_Trailing(b) = d5 * reflector(b) + trailing(b)
+      }
 
-      for (b <- 0 until m) {
-        myMatrix(b)(j) =  final_Trailing(b)
-        //println(trailing(b));
+      //hqr12
+      for (b <- jr until m) {
+        final_Trailing(b) = d5 * reflector(b) + trailing(b)
+        //print(final_Trailing(b) + " ")
       }
 
 
+      for (b <- jr until m) {
+        myMatrix(b)(jr) = final_Trailing(b)
+      }
     }
-
-
-
-
   }
 }
 object Demo {
